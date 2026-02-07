90dc877ecd86f3a7010b21d7691f2e73ee437fae
Mr_antimatter <Abhinavsinghyadav17oct@gmail.com>
2026-02-07 21:59:18 +0530

    location updated

--- ai-engine/main.py
+++ ai-engine/main.py
@@ -21,9 +21,9 @@ BACKEND_URL = 'http://localhost:3000'
 YOLO_MODEL_PATH = 'yolov8n.pt' # Standard YOLOv8 Nano
 
 # LOCATION CONFIGURATION (Hardcoded for Laptop/Camera 1)
-LATITUDE = 28.644800  
-LONGITUDE = 77.216721
-LOCATION_NAME = "Camera_01"
+LATITUDE = 23.41  
+LONGITUDE = 85.44
+LOCATION_NAME = "BIT Mesra - Camera 01"
 
 class AIEngine:
     def __init__(self):
